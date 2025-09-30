3e23a1e1d40e7e8a35aab49ef1a2fff4daa97fa0
Parsz123 <hetaldr@yahoo.com>
2025-09-30 18:28:21 +0100

    hello

---
+++ Activity 2.py
@@ -0,0 +1,7 @@
+String1 = str(input("Please enter your string:"))
+String2 = ('')
+for i in String1:
+    String2 = i + String2
+
+print(" \n The original string is", String1)
+print(" \n The new string is", String2)
